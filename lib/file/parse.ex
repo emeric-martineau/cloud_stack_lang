0ab08c8cc9f27149c7bb761db6af2d844c0c2300
Emeric MARTINEAU <11473190+bubulemaster@users.noreply.github.com>
2020-04-03 17:44:50 +0200

    Refactor Main module

---
+++ lib/file/parse.ex
@@ -0,0 +1,113 @@
+defmodule CloudStackLang.File.Parse do
+  @moduledoc ~S"""
+    This module contain some functions for write file when it's parser.
+  """
+  alias CloudStackLang.Export.AwsYaml
+
+  def parse_files(_debug, [], _output) do
+    IO.puts(:stderr, "Error! You must provide a filename.")
+    IO.puts(:stderr, "Please run 'csl --help' for more information.")
+
+    false
+  end
+
+  def parse_files(debug, files, output_format),
+    do:
+      files
+      |> Enum.filter(fn f -> not File.exists?(f) end)
+      |> manage_not_found_file(files, debug, output_format)
+
+  defp manage_not_found_file([], files, debug, output_format),
+    do: run_all_files(true, files, debug, output_format)
+
+  defp manage_not_found_file(missing_files, _files, _debug, _output_format) do
+    Enum.each(missing_files, fn f -> IO.puts(:stderr, "File #{f} not found!") end)
+
+    false
+  end
+
+  defp run_all_files(true, [current_file | tail], debug, output_format) do
+    continue_run =
+      run(debug, output_format, current_file, CloudStackLang.Functions.Base.get_map(), %{})
+
+    run_all_files(continue_run, tail, debug, output_format)
+  end
+
+  # All files are parsed with success
+  defp run_all_files(true, [], _debug, _output_format), do: true
+
+  defp run_all_files(false, _files, _debug, _output_format), do: false
+
+  defp run(debug, output_format, filename, fct, modules_fct) do
+    IO.puts("Parsing file #{filename}")
+
+    File.read!(filename)
+    |> CloudStackLang.Parser.parse_and_eval(debug, %{}, fct, modules_fct)
+    |> manage_parse_and_eval_return(filename, output_format)
+  end
+
+  defp manage_parse_and_eval_return({:error, line, msg}, filename, _output_format) do
+    IO.puts(:stderr, "Error in script '#{filename}' at line #{line}: #{msg}")
+
+    false
+  end
+
+  # TODO extension come from AwsYaml
+  # TODO support output_format '-' for stdin
+  # TODO support directory file and recursive mode -> generate only one file in this case
+  defp manage_parse_and_eval_return(map, filename, output_format),
+    do:
+      AwsYaml.gen(map[:modules])
+      |> write_result(filename, output_format, "yaml")
+
+  defp write_result(content, filename, output_format, extension) do
+    new_filename = generate_new_filename(filename, output_format, extension)
+
+    new_filename
+    |> File.write(content)
+    |> manage_write_file_error(new_filename, filename)
+  end
+
+  defp manage_write_file_error({:error, msg}, new_filename, filename) do
+    err_msg = build_error_message({:error, msg}, new_filename, filename)
+    IO.puts(:stderr, err_msg)
+
+    false
+  end
+
+  defp manage_write_file_error(_, new_filename, _filename) do
+    IO.puts("Writting file '#{new_filename}' done.")
+
+    true
+  end
+
+  defp build_error_message({:error, :enoent}, new_filename, filename),
+    do: build_error_message(new_filename, filename, "file does not exist")
+
+  defp build_error_message({:error, :enotdir}, new_filename, filename),
+    do: build_error_message(new_filename, filename, "not a directory")
+
+  defp build_error_message({:error, :enospc}, new_filename, filename),
+    do: build_error_message(new_filename, filename, "no space left on the device")
+
+  defp build_error_message({:error, :eacces}, new_filename, filename),
+    do: build_error_message(new_filename, filename, "missing permission for writing")
+
+  defp build_error_message({:error, :eisdir}, new_filename, filename),
+    do: build_error_message(new_filename, filename, "file is a directory")
+
+  defp build_error_message(error, new_filename, filename),
+    do: "Error when write file '#{new_filename}' (for '#{filename}'): #{error}"
+
+  defp generate_new_filename(filename, output_format, format) do
+    dirname = Path.dirname(filename)
+    basename = Path.basename(filename)
+    extname = Path.extname(filename)
+
+    output_format
+    |> String.replace("%dirname", dirname)
+    |> String.replace("%filename", basename)
+    |> String.replace("%extension", extname)
+    |> String.replace("%format", format)
+  end
+end
--- lib/main.ex
+++ lib/main.ex
@@ -1,7 +1,7 @@
 defmodule CloudStackLang.Main do
   @version Mix.Project.config()[:version]
 
-  alias CloudStackLang.Export.AwsYaml
+  alias CloudStackLang.File.Parse
 
   def main(args) do
     options = [
@@ -31,95 +31,18 @@ defmodule CloudStackLang.Main do
     output_format = Keyword.get(opts, :output, "%dirname/%filename.%format")
 
     cond do
-      app_version == true -> version()
-      app_help == true -> help(options)
-      true -> parse_files(debug, files, output_format)
-    end
-  end
+      app_version == true ->
+        version()
 
-  defp parse_files(_debug, [], _output) do
-    IO.puts(:stderr, "Error! You must provide a filename.")
-    IO.puts(:stderr, "Please run 'csl --help' for more information.")
+      app_help == true ->
+        help(options)
 
-    System.halt(1)
-  end
-
-  defp parse_files(debug, files, output_format) do
-    not_found_files =
-      files
-      |> Enum.filter(fn f -> not File.exists?(f) end)
-
-    case not_found_files do
-      [] ->
-        Enum.each(files, fn f ->
-          run(debug, output_format, f, CloudStackLang.Functions.Base.get_map(), %{})
-        end)
-
-        System.halt(0)
-
-      missing_files ->
-        Enum.each(missing_files, fn f -> IO.puts(:stderr, "File #{f} not found!") end)
-        System.halt(1)
-    end
-  end
-
-  defp run(debug, output_format, filename, fct, modules_fct) do
-    IO.puts("Parsing file #{filename}")
-
-    text = File.read!(filename)
-
-    ret = CloudStackLang.Parser.parse_and_eval(text, debug, %{}, fct, modules_fct)
-
-    case ret do
-      {:error, line, msg} ->
-        IO.puts(:stderr, "Error in script '#{filename}' at line #{line}: #{msg}")
-        System.halt(1)
-
-      map ->
-        content = AwsYaml.gen(map[:modules])
-        # TODO extension come from AwsYaml
-        new_filename = generate_new_filename(filename, output_format, "yaml")
-
-        # TODO support output_format '-' for stdin
-        # TODO support directory file and recursive mode
-
-        case File.write(new_filename, content) do
-          {:error, msg} ->
-            IO.puts(
-              :stderr,
-              "Error when write file '#{new_filename}' (for '#{filename}'): #{
-                get_write_error(msg)
-              }"
-            )
-
-            System.halt(1)
-
-          _ ->
-            IO.puts("Writting file '#{new_filename}' done.")
+      true ->
+        case Parse.parse_files(debug, files, output_format) do
+          true -> System.halt(0)
+          _ -> System.halt(1)
         end
     end
-  end
-
-  defp get_write_error(msg) do
-    case msg do
-      :enoent -> "file does not exist"
-      :enotdir -> "not a directory"
-      :enospc -> "no space left on the device"
-      :eacces -> "missing permission for writing"
-      :eisdir -> "file is a directory"
-    end
-  end
-
-  defp generate_new_filename(filename, output_format, format) do
-    dirname = Path.dirname(filename)
-    basename = Path.basename(filename)
-    extname = Path.extname(filename)
-
-    output_format
-    |> String.replace("%dirname", dirname)
-    |> String.replace("%filename", basename)
-    |> String.replace("%extension", extname)
-    |> String.replace("%format", format)
   end
 
   defp version() do
